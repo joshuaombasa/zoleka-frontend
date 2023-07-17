e163200907af288d5b1593f5a70bfe1197846ce0
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-07-17 22:08:18 +0300

    added initial App jsx file

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React,{ useState } from 'react'
+import Hero from './Hero'
+import './App.css'
+
+function App() {
+
+
+
+
+  return (
+    <>
+     <Hero/>
+    </>
+  )
+}
+
+export default App
