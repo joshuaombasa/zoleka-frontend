62eb63658b9b064323828e70e9b2749b1584efcc
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-07-19 20:28:58 +0300

    updated context file

--- src/context/formContextProvider.jsx
+++ src/context/formContextProvider.jsx
@@ -1,9 +1,36 @@
 import React, { createContext } from "react";
 
-
+const formContext = createContext()
 
 function FormContextProvider() {
+
+
+    function handleSubmit(formData) {
+
+        fetch('http://localhost:3000/customers', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data); // Display response from the server
+                setFormData({ name: '', location: '', country: '' }); // Reset the form
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
     return (
-        <></>
+        <>
+            <formContext.Provider>
+
+            </formContext.Provider>
+        </>
     )
 }
+
+export {FormContextProvider, formContext}
