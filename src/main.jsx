f656e9752c631657fd665b2f7482cf9e8a1b7658
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-07-19 20:33:56 +0300

    updated context file

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { formContext } from './context/formContextProvider.jsx'
+import { FormContextProvider } from './context/formContextProvider.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <formContext>
+  <FormContextProvider>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </formContext>,
+  </FormContextProvider>,
 )
